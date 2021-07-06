8b27b11b3f81e791bd11b4e43b4ff6ae1a647c76
Jojana13 <86811860+Jojana13@users.noreply.github.com>
2021-07-06 00:12:46 -0400

    Add files via upload

---
+++ Color Changing Background.js
@@ -0,0 +1,70 @@
+var btn_red;
+var btn_green;
+var btn_blue;
+var btn_yellow;
+
+
+var r = 0;
+var g = 0;
+var b = 0;
+
+
+
+function setup() {
+  createCanvas(400, 400);  
+
+  btn_red=createButton("RED");
+  btn_red.position(100,50);
+  btn_red.mousePressed(red_bg);
+
+  btn_green=createButton("GREEN");
+  btn_green.position(250,50);
+  btn_green.mousePressed(green_bg);
+
+  btn_blue=createButton("BLUE");
+  btn_blue.position(250,250);
+  btn_blue.mousePressed(blue_bg);
+
+  btn_yellow=createButton("YELLOW");
+  btn_yellow.position(100,250);
+  btn_yellow.mousePressed(yellow_bg);
+
+}
+
+function draw() {
+  background(r,g,b);
+}
+
+function red_bg()
+{
+  r= 255;
+  g= 0;
+  b= 0;
+  
+}
+
+function green_bg()
+{
+  r= 0;
+  g= 255;
+  b= 0;
+ 
+}
+
+function blue_bg()
+{
+  r= 0;
+  g= 0;
+  b= 255;
+  
+}
+
+function yellow_bg()
+{
+  r= 255;
+  g= 255;
+  b= 0;
+ 
+}
+
+
